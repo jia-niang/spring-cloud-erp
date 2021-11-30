904d27bf98db15a4698d72a35f527138b20c558c
CAREERINTLINC\CI24192 <jonathanzhang@careerintlinc.com>
2021-11-30 18:15:49 +0800

    feat: jwt

--- services/auth-service/src/main/java/com/kabunx/erp/property/JwtProperties.java
+++ services/auth-service/src/main/java/com/kabunx/erp/property/JwtProperties.java
@@ -1,6 +1,5 @@
 package com.kabunx.erp.property;
 
-import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
--- services/auth-service/src/main/java/com/kabunx/erp/util/JwtUtils.java
+++ services/auth-service/src/main/java/com/kabunx/erp/util/JwtUtils.java
@@ -17,6 +17,7 @@ import java.util.Map;
 
 @Component
 public class JwtUtils {
+
     @Resource
     JwtProperties jwtProperties;
 
