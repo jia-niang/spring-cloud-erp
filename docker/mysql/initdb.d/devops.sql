915f1e70779a52bc32c983d9b6c05ac168549b9e
CAREERINTLINC\CI24192 <jonathanzhang@careerintlinc.com>
2022-01-19 18:08:08 +0800

    feat: gitea

--- docker/mysql/initdb.d/devops.sql
+++ docker/mysql/initdb.d/devops.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE IF NOT EXISTS `devops_gitea` DEFAULT CHARSET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
+GRANT ALL ON `devops_gitea`.* TO 'admin'@'%';
+FLUSH PRIVILEGES;
+
 CREATE DATABASE IF NOT EXISTS `devops_drone` DEFAULT CHARSET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
 GRANT ALL ON `devops_drone`.* TO 'admin'@'%';
 FLUSH PRIVILEGES;
